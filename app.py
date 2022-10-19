f65bee4d831caaa9326635d7434de5c87f1d861f
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2022-10-19 21:27:07 +0300

    add1

--- app.py
+++ app.py
@@ -34,3 +34,57 @@ def get_user(uid):
         "role": user.role,
         "phone": user.phone
     })
+
+@app.route("/orders")
+def get_orders():
+        order_list = Order.query.all()
+
+        order_response = []
+
+        for order in order_list:
+            order_response.append({
+                "id": order.id,
+                "name": order.name,
+                "description": order.description,
+                "start_date": order.start_name,
+                "end_date": order.end_date,
+                "address": order.address,
+                "price": order.price
+            })
+
+        return json.dumps(order_response)
+
+
+@app.route("/orders/<int:uid>")
+def get_order(uid):
+    order = Order.query.get(uid)
+    return json.dumps({
+        "id": order.id,
+        "name": order.name,
+        "description": order.description,
+        "start_date": order.start_name,
+        "end_date": order.end_date,
+        "address": order.address,
+        "price": order.price
+    })
+
+@app.route("/offers")
+def get_offers():
+        offer_list = Offer.query.all()
+
+        offer_response = []
+
+        for offer in offer_list:
+            offer_response.append({
+                "id": offer.id,
+            })
+
+        return json.dumps(order_response)
+
+
+@app.route("/offers/<int:uid>")
+def get_offer(uid):
+    offer = Offer.query.get(uid)
+    return json.dumps({
+        "id": offer.id,
+    })
